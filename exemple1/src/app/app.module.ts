23e9f1fe388fce1629475279f18c5716fd868e37
Reda SAFIR <r.safir@cmr.gov.ma>
2022-05-30 16:18:38 +0100

    add ex1

--- exemple1/src/app/app.module.ts
+++ exemple1/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NotificationButtonComponent } from './notification-button/notification-button.component';
+import { NotificationManagerComponent } from './notification-manager/notification-manager.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NotificationButtonComponent,
+    NotificationManagerComponent
   ],
   imports: [
     BrowserModule,
---
+++ exemple1/src/app/notification-manager/notification-manager.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-notification-manager',
+  templateUrl: './notification-manager.component.html',
+  styleUrls: ['./notification-manager.component.css']
+})
+export class NotificationManagerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
